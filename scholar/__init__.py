c195bc5fa74618f3f528470bfb3d2f382ca95834
Luciano Bello <luciano@debian.org>
2013-03-24 14:52:14 +0100

    modularization

--- scholar/__init__.py
+++ scholar/__init__.py
@@ -1,1 +1,17 @@
 from scholar import searchAuthor,Author
+
+"""Fetch information from Google Scholar
+
+At the moment, the module is mainly focus in authors information fetch.
+
+Search for authors (e.g. called "Bello" working for "Chalmers University"):
+ >>> import scholar
+ >>> l=scholar.searchAuthor('Bello Chalmers').next()
+ >>> l.name
+ <<< u'Luciano Bello'
+
+Fetch all the infmation of an author:
+
+
+"""
+__version__=  '0.1'
