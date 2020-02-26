2709d765f9da3c991544623270b8dd676511624c
Jan Ruh <jan.ruh@tttech.com>
2020-02-26 15:21:26 +0100

    Import use_proxy function

--- scholarly/__init__.py
+++ scholarly/__init__.py
@@ -1,3 +1,4 @@
 from .scholarly import Author, Publication
 from .scholarly import search_author, search_keyword, search_pubs_query
 from .scholarly import search_pubs_custom_url, search_author_custom_url
+from .scholarly import use_proxy
