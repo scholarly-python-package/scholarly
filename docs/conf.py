82ec37561f081b3314e14df60b763854190126a0
silvavn <37382997+silvavn@users.noreply.github.com>
2020-04-17 21:55:39 -0600

    Fix conf to compile rtd

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ author = 'Victor N. Silva'
 
 # The full version, including alpha/beta/rc tags
 release = '0.3.0'
+master_doc = 'index'
 
 
 # -- General configuration ---------------------------------------------------
