c018e9c2ecff6985d4f7fc2cd5fcb098339563b1
Matthew <matthew.bluteau@gmail.com>
2020-04-23 17:27:43 +0100

    Amend Author.fill() to allow update by sections
    Instead of updating pulling all of the information from an author's
    profile, Author.fill() now can pull in certain sections with the
    `sections` argument. More extensive testing required. Tests fail because
    they are out of date with Google Scholar.

--- scholarly/scholarly.py
+++ scholarly/scholarly.py
@@ -259,55 +259,80 @@ class Author(object):
                 self.citedby = int(citedby.text[9:])
         self._filled = False
 
-    def fill(self):
-        """Populate the Author with information from their profile"""
+    def fill(self, sections=['all']):
+        """Populate the Author with information from their profile
+
+        The `sections` argument allows for finer granularity of the profile
+        information to be pulled.
+
+        Parameters
+        ----------
+        sections : list of str
+            The sections of author information that should be filled. They
+            are broken down as follows:
+            'basic' = name, affiliation, and interests;
+            'citation_indices' = h-index, i10-index, and 5-year analogues;
+            'num_citations' = number of citations per year;
+            'co-authors' = co-authors;
+            'publications' = publications;
+            'all' = all of the above (this is the default)
+        """
+        sections = [section.lower() for section in sections]
+        print(sections)
         url_citations = _CITATIONAUTH.format(self.id)
         url = '{0}&pagesize={1}'.format(url_citations, _PAGESIZE)
         soup = _get_soup(_HOST+url)
-        self.name = soup.find('div', id='gsc_prf_in').text
-        self.affiliation = soup.find('div', class_='gsc_prf_il').text
-        self.interests = [i.text.strip() for i in soup.find_all('a', class_='gsc_prf_inta')]
+        if 'basic' in sections or 'all' in sections:
+            self.name = soup.find('div', id='gsc_prf_in').text
+            self.affiliation = soup.find('div', class_='gsc_prf_il').text
+            self.interests = [i.text.strip() for i in
+                              soup.find_all('a', class_='gsc_prf_inta')]
         
         # h-index, i10-index and h-index, i10-index in the last 5 years
-        index = soup.find_all('td', class_='gsc_rsb_std')
-        if index:
-            self.citedby = int(index[0].text)
-            self.citedby5y = int(index[1].text)
-            self.hindex = int(index[2].text)
-            self.hindex5y = int(index[3].text)
-            self.i10index = int(index[4].text)
-            self.i10index5y = int(index[5].text)
-        else:
-            self.hindex = self.hindex5y = self.i10index = self.i10index5y = 0
+        if 'citation_indices' in sections or 'all' in sections:
+            index = soup.find_all('td', class_='gsc_rsb_std')
+            if index:
+                self.citedby = int(index[0].text)
+                self.citedby5y = int(index[1].text)
+                self.hindex = int(index[2].text)
+                self.hindex5y = int(index[3].text)
+                self.i10index = int(index[4].text)
+                self.i10index5y = int(index[5].text)
+            else:
+                self.hindex = self.hindex5y = self.i10index = self.i10index5y = 0
 
         # number of citations per year
-        years = [int(y.text) for y in soup.find_all('span', class_='gsc_g_t')]
-        cites = [int(c.text) for c in soup.find_all('span', class_='gsc_g_al')]
-        self.cites_per_year = dict(zip(years, cites))
+        if 'citation_num' in sections or 'all' in sections:
+            years = [int(y.text) for y in soup.find_all('span', class_='gsc_g_t')]
+            cites = [int(c.text) for c in soup.find_all('span', class_='gsc_g_al')]
+            self.cites_per_year = dict(zip(years, cites))
 
         # co-authors
-        self.coauthors = []
-        for row in soup.find_all('span', class_='gsc_rsb_a_desc'):
-            new_coauthor = Author(re.findall(_CITATIONAUTHRE, row('a')[0]['href'])[0])
-            new_coauthor.name = row.find(tabindex="-1").text
-            new_coauthor.affiliation = row.find(class_="gsc_rsb_a_ext").text
-            self.coauthors.append(new_coauthor)
+        if 'co-authors' in sections or 'all' in sections:
+            self.coauthors = []
+            for row in soup.find_all('span', class_='gsc_rsb_a_desc'):
+                new_coauthor = Author(re.findall(_CITATIONAUTHRE, row('a')[0]['href'])[0])
+                new_coauthor.name = row.find(tabindex="-1").text
+                new_coauthor.affiliation = row.find(class_="gsc_rsb_a_ext").text
+                self.coauthors.append(new_coauthor)
 
 
-        self.publications = list()
-        pubstart = 0
-        while True:
-            for row in soup.find_all('tr', class_='gsc_a_tr'):
-                new_pub = Publication(row, 'citations')
-                self.publications.append(new_pub)
-            if 'disabled' not in soup.find('button', id='gsc_bpf_more').attrs:
-                pubstart += _PAGESIZE
-                url = '{0}&cstart={1}&pagesize={2}'.format(url_citations, pubstart, _PAGESIZE)
-                soup = _get_soup(_HOST+url)
-            else:
-                break
+        if 'publications' in sections or 'all' in sections:
+            self.publications = list()
+            pubstart = 0
+            while True:
+                for row in soup.find_all('tr', class_='gsc_a_tr'):
+                    new_pub = Publication(row, 'citations')
+                    self.publications.append(new_pub)
+                if 'disabled' not in soup.find('button', id='gsc_bpf_more').attrs:
+                    pubstart += _PAGESIZE
+                    url = '{0}&cstart={1}&pagesize={2}'.format(url_citations, pubstart, _PAGESIZE)
+                    soup = _get_soup(_HOST+url)
+                else:
+                    break
 
-        self._filled = True
+        if 'all' in sections:
+            self._filled = True
         return self
 
     def __str__(self):
