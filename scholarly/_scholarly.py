b0471e3c9534f4359657ad7263305f2318ae366f
silvavn <37382997+silvavn@users.noreply.github.com>
2020-05-21 18:08:44 -0600

    Updated documenation
    Changed documentation to sphinx format to be compatible with autodocs.
    
    Changed wording to be come concise
    
    Changed some annotations to be more summarized or to use different terminology
    
    made code pep8 compliant.

--- scholarly/_scholarly.py
+++ scholarly/_scholarly.py
@@ -9,75 +9,91 @@ _PUBSEARCH = '/scholar?hl=en&q={0}'
 
 class _Scholarly(object):
     """docstring for scholarly"""
+
     def __init__(self):
         self.__nav = Navigator()
 
     def use_proxy(self, http: str, https: str):
-        """
-        Defines a proxy to use for the network connection.
-        If you use a Tor proxy without the appropriate setup for refreshing
-        the id, then you can use this function to set the Tor SOCK proxy as the
-        proxy for scholarly.
-        """
-        self.nav._use_proxy(http, https)
+        """Setups a proxy without refreshing capabilities.
 
-    def use_tor(self, tor_sock_port: int, tor_control_port: int, tor_password: str):
+        :param http: the http proxy address
+        :type http: str
+        :param https: the https proxy address
+        :type https: str
         """
-        Use this function to use Tor, where it is possible to access a Control Port
-        for refreshing the ID. If you do not have access to the control port,
-        then simply set the proxy using the `use_proxy` method.
 
-        Args:
-            :param tor_sock_port: defines the port where the sock proxy will run.
-            :param tor_control_port: defines the port where the control port will run.
-            :param tor_password: the password for authenticating against the control port
-        Returns:
-            A dictionary whether the proxy works, and whether it is possible to refresh the id
-        Example:
+        self.__nav._use_proxy(http, https)
+
+    def use_tor(self, tor_sock_port: int, tor_control_port: int, tor_pw: str):
+        """[summary]
+
+        [description]
+        :param tor_sock_port: Tor sock proxy port.
+        :type tor_sock_port: int
+        :param tor_control_port: Tor controller port.
+        :type tor_control_port: int
+        :param tor_pw: Tor controller password
+        :type tor_pw: str
+
+        :Example::
+
             scholarly.use_tor(9050, 9051, "scholarly_password")
-        """
-        self.nav._setup_tor(tor_sock_port, tor_control_port, tor_password)
 
-    def launch_tor(self, tor_cmd: str, tor_sock_port=None, tor_control_port=None):
         """
-        Use this method to launch a temporary Tor proxy that will be used only
-        by scholarly. It is necessary to pass the location of the Tor executable
-        in the `tor_cmd` parameter
+        self.__nav._setup_tor(tor_sock_port, tor_control_port, tor_pw)
 
-        Args:
-            :param tor_cmd: points to the Tor executable in your system
-            :param tor_sock_port: (optional) defines the port where the sock proxy will run.
-            :param tor_control_port: (optional) defines the port where the control port will run.
-        Returns:
-            A dictionary showing the ports where the Tor server is running,
-            whether the proxy works, and whether it is possible to refresh the id
-        Example:
+    def launch_tor(self,
+                   tor_path: str, tor_sock_port: int, tor_control_port: int):
+        """Launches a temporary Tor connection
+
+        Launches a temporary Tor connection to be used by scholarly. 
+        This method requires the absolute path to a Tor executable file.
+
+        :param tor_path: Absolute path to the local Tor binary
+        :type tor_path: str
+        :param tor_sock_port: Tor sock proxy port.
+        :type tor_sock_port: int
+        :param tor_control_port: Tor controller port.
+        :type tor_control_port: int
+
+        :Example::
+
             scholarly.launch_tor('/usr/bin/tor')
         """
-        self.nav._launch_tor(tor_cmd, tor_sock_port, tor_control_port)
+        self.__nav._launch_tor(tor_path, tor_sock_port, tor_control_port)
 
-    def search_pubs(self, query, patents=True, citations=True, year_low=None, year_high=None):
-        """
-        Searches by query and returns a generator of Publication objects
+    def search_pubs(self,
+                    query: str, patents: bool = True,
+                    citations: bool = True, year_low: int = None,
+                    year_high: int = None):
+        """Searches by query and returns a generator of Publication objects
 
-        search_pubs(query, patents=True, year_low=None, year_high=None)
-        Args:
-            query: string with search terms
-            patents: bool, whether to include patents in search results (default True)
-            citations: bool, whether to include citations in search results (default True)
-            year_low: int, if given, earliest year from which to include results (default None)
-            year_high: int, if given, latest year from which to include results (default None)
-        Returns:
-            generator object with search results
-        Example:
+        [description]
+        :param query: terms to be searched
+        :type query: str
+        :param patents: Whether or not to include patents, defaults to True
+        :type patents: bool, optional
+        :param citations: Whether or not to include citations, defaults to True
+        :type citations: bool, optional
+        :param year_low: minimum year of publication, defaults to None
+        :type year_low: int, optional
+        :param year_high: maximum year of publication, defaults to None
+        :type year_high: int, optional
+        :returns: Generator of Publication objects
+        :rtype: Iterator[:class:`Publication`]
+
+        :Example::
+
             s = scholarly.search_pubs_query('cancer', year_low=2015)
         """
         url = _PUBSEARCH.format(requests.utils.quote(query))
 
         yr_lo = '&as_ylo={0}'.format(year_low) if year_low is not None else ''
-        yr_hi = '&as_yhi={0}'.format(year_high) if year_high is not None else ''
+        yr_hi = '&as_yhi={0}'.format(
+            year_high) if year_high is not None else ''
         citations = '&as_vis={0}'.format(1 - int(citations))
         patents = '&as_sdt={0},33'.format(1 - int(patents))
+        # improve str below
         url = url + yr_lo + yr_hi + citations + patents
         return self.nav.search_publications(url)
 
