scholarly-python-package/scholarly

commit 68b295b43770346e1732297941742aa32479958d

Fix parsing of venues with commas, NBSPs and ampersands
Also, remove stray spaces in years.

scholarly/publication.py
@@ -88,8 +88,7 @@ class Publication(object):
 
     def _get_authorlist(self, authorinfo):
         authorlist = list()
-        text = authorinfo.text.replace(u'\xa0', u' ')
-        text = text.split(' - ')[0]
+        text = authorinfo.split(' - ')[0]
         for i in text.split(','):
             i = i.strip()
             if bool(re.search(r'\d', i)):
@@ -121,12 +120,16 @@ class Publication(object):
         if title.find('a'):
             self.bib['url'] = title.find('a')['href']
 
-        authorinfo = databox.find('div', class_='gs_a')
+        authorinfo = databox.find('div', class_='gs_a').text
+        authorinfo = authorinfo.replace(u'\xa0', u' ')       # NBSP
+        authorinfo = authorinfo.replace(u'&amp;', u'&')      # Ampersand
         self.bib["author"] = self._get_authorlist(authorinfo)
 
         try:
-            self.bib['venue'], self.bib['year'] = authorinfo.text.split(
-                ' - ')[1].split(',')
+            venueyear = authorinfo.split(' - ')[1].split(',')
+            self.bib['venue'] = ''.join(venueyear[0:-1])
+            self.bib['year'] = venueyear[-1]
+            self.bib['year'] = self.bib['year'].strip()
         except Exception:
             self.bib['venue'], self.bib['year'] = 'NA', 'NA'
 

commit bf2f978c3cddbaaead602ca8483b3476c79f15b1

Fix the fix for parsing venues and years
This change fixes some regressions introduced by me in
68b295b43770346e1732297941742aa32479958d

scholarly/publication.py
@@ -125,13 +125,31 @@ class Publication(object):
         authorinfo = authorinfo.replace(u'&amp;', u'&')      # Ampersand
         self.bib["author"] = self._get_authorlist(authorinfo)
 
-        try:
-            venueyear = authorinfo.split(' - ')[1].split(',')
-            self.bib['venue'] = ''.join(venueyear[0:-1])
-            self.bib['year'] = venueyear[-1]
-            self.bib['year'] = self.bib['year'].strip()
-        except Exception:
+        # There are 4 (known) patterns in the author/venue/year/host line:
+        #  (A) authors - host
+        #  (B) authors - venue, year - host
+        #  (C) authors - venue - host
+        #  (D) authors - year - host
+        # The authors are handled above so below is only concerned with
+        # the middle venue/year part. In principle the venue is separated
+        # from the year by a comma. However, there exist venues with commas
+        # and as shown above there might not always be a venue AND a year...
+        venueyear = authorinfo.split(' - ')
+        # If there is no middle part (A) then venue and year are unknown.
+        if len(venueyear) <= 2:
             self.bib['venue'], self.bib['year'] = 'NA', 'NA'
+        else:
+            venueyear = venueyear[1].split(',')
+            venue = 'NA'
+            year = venueyear[-1].strip()
+            if year.isnumeric() and len(year) == 4:
+                self.bib['year'] = year
+                if len(venueyear) >= 2:
+                    venue = ','.join(venueyear[0:-1]) # everything but last
+            else:
+                venue = ','.join(venueyear) # everything
+                self.bib['year'] = 'NA'
+            self.bib['venue'] = venue
 
         if databox.find('div', class_='gs_rs'):
             self.bib['abstract'] = databox.find('div', class_='gs_rs').text