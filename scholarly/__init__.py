08e33c75615c71c43f49330abc3a73258eac3f60
arunkannawadi <kj.arun.kj@gmail.com>
2021-09-15 19:27:08 -0400

    Exposure scholarly exceptions

--- scholarly/__init__.py
+++ scholarly/__init__.py
@@ -1,4 +1,5 @@
 from ._scholarly import _Scholarly
 from ._proxy_generator import ProxyGenerator
 from .data_types import Author, Publication
+from ._navigator import DOSException, MaxTriesExceededException
 scholarly = _Scholarly()
