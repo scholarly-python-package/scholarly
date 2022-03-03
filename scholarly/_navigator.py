474022380ada1a836cff1806c889a4af2ed70393
arunkannawadi <kj.arun.kj@gmail.com>
2022-03-02 23:00:40 -0500

    Switch to premium proxy after timeout
    Fixes #392.

--- scholarly/_navigator.py
+++ scholarly/_navigator.py
@@ -163,7 +163,12 @@ class Navigator(object, metaclass=Singleton):
                 self.logger.info("Retrying with a new session.")
 
             tries += 1
-            session, timeout = pm.get_next_proxy(num_tries = tries, old_timeout = timeout, old_proxy=session.proxies.get('http', None))
+            try:
+                session, timeout = pm.get_next_proxy(num_tries = tries, old_timeout = timeout, old_proxy=session.proxies.get('http', None))
+            except Exception:
+                self.logger.info("No other secondary connections possible. "
+                                 "Using the primary proxy for all requests.")
+                break
 
         # If secondary proxy does not work, try again primary proxy.
         if not premium:
